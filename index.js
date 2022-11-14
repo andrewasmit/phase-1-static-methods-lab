66910fb293d933706b987ebeb0ea75cc66492c04
andrew <andrewasmit@hotmail.com>
2022-11-14 09:49:15 -0600

    commit tree 5aa967fa1625f79dd34b7276871e0b3ef7a9114e

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    return str.substring(0,1).toUpperCase() + str.slice(1);
+  }
+
+
+  static sanitize(str){
+    return str.replace(/[^a-z0-9-' ]/gi, '');
+  }
+
+
+  static titleize(str){
+    const nonCaps = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from'];
+    let newArr = [];
+    let newStr = str.split(' ');
+    // console.log(newStr)
+    newArr.push(newStr[0].substring(0,1).toUpperCase() + newStr[0].slice(1));
+    // console.log(newArr);
+    for (const word of newStr){
+      if (nonCaps.includes(word)){
+          newArr.push(word)
+      } else newArr.push(word.substring(0,1).toUpperCase() + word.slice(1));
+    } newArr.splice(1,1);
+    // console.log(newArr);
+    return newArr.join(' ')
+  }
 }
+
+
+let str = 'the test is as follows. hello! my an but name is andrew';
+Formatter.titleize(str)
